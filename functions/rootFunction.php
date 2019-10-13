37f69dcf77141e4dc56358da494c065bd68fa2c5
Ainis Sitkauskas <ainis795@gmail.com>
2019-10-13 18:56:34 +0300

    Fixed autoloading and root namespace

--- functions/rootFunction.php
+++ functions/rootFunction.php
@@ -1,13 +1,11 @@
 <?php
 
-namespace root;
-
 function calculateHomeWorkSum(...$numbers) {
 
     $sum = 0;
     foreach ($numbers as $number) {
         $sum += $number;
     }
-    
+
     return $sum;
 }
--- index.php
+++ index.php
@@ -1,24 +1,19 @@
 <?php
 
-use function root\calculateHomeWorkSum;
-use function Nfq\Akademija\NotTyped\calculateHomeWorkSum as calculateNotTypedHomeWorkSum;
+use function Nfq\Akademija\NotTyped\calculateHomeWorkSum;
 use function Nfq\Akademija\Soft\calculateHomeWorkSum as calculateSoftHomeWorkSum;
 use function Nfq\Akademija\Strict\calculateHomeWorkSum as calculateStrictHomeWorkSum;
 
 require("vendor/autoload.php");
-require("functions/rootFunction.php");
-require("functions/notTypedFunction.php");
-require("functions/softFunction.php");
-require("functions/strictFunction.php");
 
 try {
 
     $colors = new Wujunze\Colors();
 
-    $sum = calculateHomeWorkSum(3, 2.2, '1');
+    $sum = \calculateHomeWorkSum(3, 2.2, '1');
     echo $colors->getColoredString("calculateHomeWorkSum: $sum \n", "green");
 
-    $sum = calculateNotTypedHomeWorkSum(3, 2.2, '1');
+    $sum = calculateHomeWorkSum(3, 2.2, '1');
     echo $colors->getColoredString("Nfq\Akademija\NotTyped\calculateHomeWorkSum: $sum \n", "red");
 
     $sum = calculateSoftHomeWorkSum(3, 2.2, '1');
